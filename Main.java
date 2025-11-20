7b0d798a8f663880b59f4df150d760d541d4b7e3
erikgm02 <erikgm02@hotmail.com>
2025-11-19 18:53:47 -0600

    Update Main.java
    parte 1

--- Main.java
+++ Main.java
@@ -1,18 +1,34 @@
-public class Main
-{
-	public static void main(String[] args) {
-		int filas = 12;
+
+import java.util.Scanner; 
+
+public class Main {
+    public static void main(String[] args) {
+        int filas = 12;
         int columnas = 16;
-		char [][] mapa = new char[filas][columnas];
-		for(int r = 0; r < filas; r++){
-		    for(int c = 0; c < columnas; c++){
-		        mapa[r][c] = '*';
-		    }
-		}
-		HiloGusano gus = new HiloGusano(mapa);
-		MonitorMapa monitor = new MonitorMapa(mapa);
-		System.out.println("Iniciando simulación.");
-		gus.start();
-		monitor.start();
-	}
+        char[][] mapa = new char[filas][columnas];
+
+
+        for (int r = 0; r < filas; r++) {
+            for (int c = 0; c < columnas; c++) {
+                mapa[r][c] = '*';
+            }
+        }
+
+
+        Scanner leer = new Scanner(System.in);
+        System.out.print("¿Cuántos gusanos quieres crear? ");
+        int cantidadGusanos = leer.nextInt();
+
+
+        MonitorMapa monitor = new MonitorMapa(mapa);
+        monitor.start();
+
+        System.out.println("Iniciando simulación con " + cantidadGusanos + " gusanos.");
+
+
+        for (int i = 0; i < cantidadGusanos; i++) {
+            HiloGusano gus = new HiloGusano(mapa);
+            gus.start();
+        }
+    }
 }
