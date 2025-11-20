93d5cef509e6fd2f3e2c16dc826823b69bd9860a
erikgm02 <erikgm02@hotmail.com>
2025-11-19 19:10:22 -0600

    Update Main.java
    PARTE FINAL

--- Main.java
+++ Main.java
@@ -1,33 +1,43 @@
-
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int filas = 12;
-        int columnas = 16;
+        Scanner leer = new Scanner(System.in);
+        
+
+        System.out.println("--- Configuración del Jardín ---");
+        System.out.print("Ingrese número de filas para el jardín: ");
+        int filas = leer.nextInt();
+        
+        System.out.print("Ingrese número de columnas para el jardín: ");
+        int columnas = leer.nextInt();
+        
+
         char[][] mapa = new char[filas][columnas];
+        
 
-
-        for (int r = 0; r < filas; r++) {
-            for (int c = 0; c < columnas; c++) {
+        for(int r = 0; r < filas; r++){
+            for(int c = 0; c < columnas; c++){
                 mapa[r][c] = '*';
             }
         }
-
-
-        Scanner leer = new Scanner(System.in);
+        
         System.out.print("¿Cuántos gusanos quieres crear? ");
         int cantidadGusanos = leer.nextInt();
+        
+
+        System.out.print("Ingresa la velocidad (tiempo en ms): ");
+        int velocidad = leer.nextInt();
 
 
         MonitorMapa monitor = new MonitorMapa(mapa);
+        System.out.println("Iniciando simulación...");
+        
         monitor.start();
-
-        System.out.println("Iniciando simulación con " + cantidadGusanos + " gusanos.");
-
-
-        for (int i = 0; i < cantidadGusanos; i++) {
-            HiloGusano gus = new HiloGusano(mapa);
+        
+        for(int i = 0; i < cantidadGusanos; i++){
+    
+            HiloGusano gus = new HiloGusano(mapa, velocidad);
             gus.start();
         }
     }
