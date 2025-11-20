335339d3f534b2a0c9eac764c610bec4a40bf3c9
erikgm02 <erikgm02@hotmail.com>
2025-11-19 18:53:21 -0600

    Update HiloGusano.java
    parte 1

--- HiloGusano.java
+++ HiloGusano.java
@@ -1,75 +1,99 @@
-public class HiloGusano extends Thread{
+
+public class HiloGusano extends Thread {
     private char[][] jardin;
     private int filas;
     private int columnas;
-    public HiloGusano (char[][] mundo){
+
+    public HiloGusano(char[][] mundo) {
         this.jardin = mundo;
         if (mundo != null && mundo.length > 0) {
-             this.filas = jardin.length;
-             this.columnas = jardin[0].length;
+            this.filas = jardin.length;
+            this.columnas = jardin[0].length;
         } else {
-             this.filas = 0;
-             this.columnas = 0;
+            this.filas = 0;
+            this.columnas = 0;
         }
     }
-    public void caminaRenglon(int r){
+    
+    private boolean quedaJardin() {
+        synchronized (jardin) {
+            for (int r = 0; r < filas; r++) {
+                for (int c = 0; c < columnas; c++) {
+                    if (jardin[r][c] == '*') {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false; 
+    }
+
+    public void caminaRenglon(int r) {
         if (r >= 0 && r < filas) {
-            for(int x = 0; x < columnas; x++){
-                jardin[r][x] = 'W';
+            for (int x = 0; x < columnas; x++) {
+
+                jardin[r][x] = 'W'; 
             }
         }
     }
-    public void caminaColumna(int c){
+
+    public void caminaColumna(int c) {
         if (c >= 0 && c < columnas) {
-            for(int x = 0; x < filas; x++){
+            for (int x = 0; x < filas; x++) {
                 jardin[x][c] = 'W';
             }
         }
     }
-    public void comerColumna(int c, int traga){
+
+    public void comerColumna(int c, int traga) {
         if (c >= 0 && c < columnas) {
-            int limite = Math.min(traga, filas); 
-            for(int x = 0; x < limite; x++){
+            int limite = Math.min(traga, filas);
+            for (int x = 0; x < limite; x++) {
                 jardin[x][c] = 'C';
             }
         }
     }
-    public void comerRenglon(int r, int traga){
+
+    public void comerRenglon(int r, int traga) {
         if (r >= 0 && r < filas) {
             int limite = Math.min(traga, columnas);
-            for(int x = 0; x < limite; x++){
+            for (int x = 0; x < limite; x++) {
                 jardin[r][x] = 'C';
-            }       
+            }
         }
     }
+
     @Override
-    public void run(){
-        int ini = 0;
-        int vidas = 75; 
-        while(vidas > 0){
-            try{
+    public void run() {
+
+        int ini = (int) (Math.random() * 100); 
+
+
+        while (quedaJardin()) {
+            try {
                 int columnaActual = ini % columnas;
-                synchronized(jardin){
+                synchronized (jardin) {
                     caminaColumna(columnaActual);
                     ini++;
                 }
+
                 sleep(1000);
- 
+
                 int renglonActual = ini % filas;
                 int tragaRenglon = 3;
- 
-                synchronized(jardin){
+
+                synchronized (jardin) {
                     comerRenglon(renglonActual, tragaRenglon);
                     ini++;
                 }
                 sleep(600);
-                vidas--;
-            } catch (InterruptedException e){
+
+            } catch (InterruptedException e) {
                 System.out.println("Interrupcion");
-                Thread.currentThread().interrupt(); 
+                Thread.currentThread().interrupt();
                 return;
             }
         }
+        System.out.println("Un gusano ha terminado su trabajo (murió).");
     }
 }
-
